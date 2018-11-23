6dc5f080d846d1296fab73b56230d66b2c7e900d
Hohyun Jun <jhh5154@naver.com>
2018-11-23 11:26:49 +0900

    added FilmNoirGenres.java

---
+++ HadoopMR/FilmNoirGenres.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class FilmNoirGenres {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        // Create a new job
+        Job job = Job.getInstance(conf, "Film-Noir Genres Count");
+
+        // Use the WordCount.class file to point to the job jar
+        job.setJarByClass(FilmNoirGenres.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        // Setting the input and output locations
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // Submit the job and wait for it's completion
+        job.waitForCompletion(true);
+    }
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private final static IntWritable zero = new IntWritable(0);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] fields = line.split(",");
+            String lastField = fields[fields.length - 1];
+            String[] genres = lastField.split("\\|");
+            Bool FilmNoirChecking = false;
+
+            for (String genre : genres) {
+                if(genre == "Film-Noir"){
+                    FilmNoirChecking = true;
+                    break;
+                }
+            }
+
+            word.set("Film-Noir");
+            if(FilmNoirChecing){
+                context.write(word, one);
+            }else{
+                context.write(word, zero);
+            }
+            
+        }
+    }
+
+    public static class Reduce extends Reducer<IntWritable, Text, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+
+            // Sum all the occurrences of the Film-Noir (key)
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+}
